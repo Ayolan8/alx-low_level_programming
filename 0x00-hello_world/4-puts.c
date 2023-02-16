90a7e13276aa78cd871e5645131e5fc4dae0835d
Ijalana Afolabi <iijalana@gmail.com>
2023-02-16 06:36:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/* This is the main function. Every program in C starts here */
+/* This is the main function that handles the  program in C starts here */
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
