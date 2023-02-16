c6ac5d3d453bd9833440922499e539804e332f36
Ijalana Afolabi <iijalana@gmail.com>
2023-02-16 06:23:40 -0800

    C is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* This is the main function. Every program in C starts here */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts(" \"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
