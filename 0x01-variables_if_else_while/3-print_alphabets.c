03c95938e2d50ed7fb1b951f6ecfe7ac374e2eb7
Ijalana Afolabi <iijalana@gmail.com>
2023-02-17 15:57:19 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - A C program that prints alphabet in lower case
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+		putchar(toupper(x));
+		putchar('\n');
+	}
+	return (0);
+}
