22b1eceb1f866680617e1d1be50d1ee147c7a782
Ijalana Afolabi <iijalana@gmail.com>
2023-02-16 17:06:14 -0800

    BETTY IS frustrating

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-/* main - a C program that prints the size of various types on the computer.*/
-/* Your program should return 0 */
+/** 
+* main - A c program thatprints out the size of various types on the computer
+* Return: 0 (success)
+*/ 
 int main(void)
 {
-
-	int a;
-	char b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a int: %lu.\n" (unsigned long)sizeof(a));
-	printf("Size of a char: %lu.\n" (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu.\n" (unsigned long)sizeof(c));
-	printf("Size of long long int: %lu.\n" (unsigned long)sizeof(d)); 
-	printf("Size of a float: %lu.\n" (unsigned long)sizeof(e));
-
-	return (0);
+int a;
+char b;
+double d;
+float e;
+printf("Size of int: %zu bytes\n", sizeof(a));
+printf("Size of char: %zu bytes\n", sizeof(b));
+printf("Size of double: %zu byte\n", sizeof(d));
+printf("Zize of float: %zu byte\n", sizeof(e));
+return (0);
 }
