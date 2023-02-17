2b67310e7376a73593ee198fbe369244693e178a
Ijalana Afolabi <iijalana@gmail.com>
2023-02-16 17:57:11 -0800

    betty why?

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 /** 
-*main - A c program thatprints out the size of various types on the computer
-*Return: 0 (success)
-*/ 
+ *main - a C programme that prints the size of various types on the computer
+ *Return: 0 (Success)
+ */
 int main(void)
 {
-int a;
-char b;
-double d;
-float e;
-printf("Size of int: %zu bytes\n", sizeof(a));
-printf("Size of char: %zu bytes\n", sizeof(b));
-printf("Size of double: %zu byte\n", sizeof(d));
-printf("Zize of float: %zu byte\n", sizeof(e));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of long int: %zu byte\n", sizeof(long int));
+printf("Zize of long long int: %zu byte\n", sizeof(long long int));
+printf("Size of float: %zu bytes\n", sizeof(float));
 return (0);
 }
