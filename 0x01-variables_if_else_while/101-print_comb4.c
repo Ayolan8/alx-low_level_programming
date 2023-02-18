d8e5b96b2cbcca9b68194b06791bcbd0fd4be114
Ijalana Afolabi <iijalana@gmail.com>
2023-02-18 15:20:59 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints all possible different number
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int o;
+	int t;
+	int h;
+
+	for (h = 0; h < 9; h++)
+	{
+		for (t = 0; t < 9; t++)
+		{
+			for (o = 0; o < 9; o++)
+			{
+				if (!((o == t) || (t == h) || (t > o) || (h > t)))
+				{
+					putchar('0' + h);
+					putchar('0' + t);
+					putchar('0' + o);
+					if (!(o == 9 && h == 7 && t == 8))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
