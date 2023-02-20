f6d58e09b20b46cb41251678bd2443033065523d
Ijalana Afolabi <iijalana@gmail.com>
2023-02-20 12:26:16 +0000

    size64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 printf("Size of char: %zu byte\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long int: %zu bytes\n", sizeof(long int));
-printf("Zize of long long int: %zu bytes\n", sizeof(long long int));
+printf("Size of long long int: %zu bytes\n", sizeof(long long int));
 printf("Size of float: %zu bytes\n", sizeof(float));
 return (0);
 }
