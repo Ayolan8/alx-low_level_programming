830c2c6f29f10be1b8f90c5c997632d109b8d0a4
Ijalana Afolabi <iijalana@gmail.com>
2023-02-20 12:23:43 +0000

    size64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-/** 
+/**
  *main - a C programme that prints the size of various types on the computer
  *Return: 0 (Success)
  */
 int main(void)
 {
+printf("Size of char: %zu byte\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of long int: %zu byte\n", sizeof(long int));
-printf("Zize of long long int: %zu byte\n", sizeof(long long int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Zize of long long int: %zu bytes\n", sizeof(long long int));
 printf("Size of float: %zu bytes\n", sizeof(float));
 return (0);
 }
