cc19473a5084c270034db1dab1284ad93a393de4
Ijalana Afolabi <iijalana@gmail.com>
2023-02-21 14:02:27 -0800

    Betty is a bitch

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int add() - function that adds two integers
+ * int x - parameter of add() function
+ * int y - parameter of add() function
+ * Return: result (success)
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
