9a4973a67eb12b7b7d8587881a6d8e10fe6e8ab3
Ijalana Afolabi <iijalana@gmail.com>
2023-02-21 09:03:52 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet in lowercase
+ * done by Ijalana
+ * Return: o upon successful
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'z'; a <= 'z'; a++)
+	{
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+}
