e50b163eddc8c47b5ee2d7ddfcac4821157ea304
Ijalana Afolabi <iijalana@gmail.com>
2023-02-21 14:30:00 -0800

    ou

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * Return: 1 or 0 (success)
+ * main - islower(int c) function
+ */
+int _islower(int c)
+{
+	int x = 't';
+
+	if (c == x)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
