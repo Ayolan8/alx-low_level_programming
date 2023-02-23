b77629ed91e38be8e8e6ee26484696590eae01e6
Ijalana Afolabi <iijalana@gmail.com>
2023-02-23 11:35:38 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - A function that checks for uppercase
+ * @c: a parameter to be returned
+ * Return: 1 if successful
+ * 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+void _putchar(void);
+int _isupper(int c);
+
+#endif
