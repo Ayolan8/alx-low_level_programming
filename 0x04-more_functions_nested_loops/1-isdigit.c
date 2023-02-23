667e87a003759f55c3e7bdd4558cb81fee3a594e
Ijalana Afolabi <iijalana@gmail.com>
2023-02-23 14:50:46 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - a function that checks for a digit
+ * @c: parameter to be printed
+ * Return: 1 if successful
+ * 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
