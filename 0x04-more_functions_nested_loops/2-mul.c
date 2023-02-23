f07a2c359b34b9acae5c0fd4b81eeabb974e36f5
Ijalana Afolabi <iijalana@gmail.com>
2023-02-23 15:01:48 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies
+ * @a: parameter to be printed
+ * @b: parameter to be printed
+ * Return: res
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
