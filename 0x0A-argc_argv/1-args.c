2bbd38dc70b4e1de9fa46f938954c1b0488d977e
Ijalana Afolabi <iijalana@gmail.com>
2023-03-10 15:49:13 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: number of argument to be entered
+ * @argv: array to the argument
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
