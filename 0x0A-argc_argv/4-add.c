0d31a22fb9c1f7ce4a6f4c4c7b7f9d8fce8c99ef
Ijalana Afolabi <iijalana@gmail.com>
2023-03-10 16:52:12 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry Point
+ * @argc: number of argument to be printed
+ * @argv: array pointing to the argument to be printed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+		return (0);
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
