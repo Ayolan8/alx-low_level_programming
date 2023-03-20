c4849ab2738a99cfba441ac37903681f2613c658
Ijalana Afolabi <iijalana@gmail.com>
2023-03-20 11:09:22 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef ABS_H
-#define ABS_H
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
 
 #define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
-#endif /* ABS_H */
+#endif /* FUNCTION_LIKE_MACRO */
